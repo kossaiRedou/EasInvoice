8dd9a7164bd5b5e6bcd2b9896b51fbbe981879db
Aibykalmaya <aibykalmaya@gmail.com>
2025-10-28 20:25:09 +0100

    setting

--- EasInvoice/settings.py
+++ EasInvoice/settings.py
@@ -14,16 +14,6 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=2k1u0$ppo+b#tc9h!vz@kht$bx&f*#&#07%z#fxtf^e4s8o70'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
